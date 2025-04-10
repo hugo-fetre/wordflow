a794d11f5d9bacd8b317a3b5afa6b6a4651b3dae
Hugo_le_developpeur <hugo.fetre@gmail.com>
2025-04-10 14:03:54 +0200

    fix type param props v4

--- app/(root)/app/[workspaceId]/page.tsx
+++ app/(root)/app/[workspaceId]/page.tsx
@@ -1,17 +1,26 @@
+// page.tsx
 import BusinessInfoForm from '@/components/shared/business_info_form'
-import MainNav from '@/components/shared/nav';
+import MainNav from '@/components/shared/nav'
 import React from 'react'
 
-const dashboardPage = ({ params }: { params: { workspaceId: string } }) => {
+// Correction : Le type 'params' doit être défini comme un objet de type { workspaceId: string }
+// et non comme une promesse ou une structure incorrecte.
 
+type SearchParamProps = {
+  params: { workspaceId: string }
+}
+
+const DashboardPage = ({ params }: SearchParamProps) => {
+  const { workspaceId } = params;
+  
   return (
     <div>
       <div className="mainWindow">
-        <BusinessInfoForm id={params.workspaceId}></BusinessInfoForm>                     
+        <BusinessInfoForm id={workspaceId}></BusinessInfoForm>                     
       </div>
-      <MainNav currentWorkspaceId={params.workspaceId} />
+      <MainNav currentWorkspaceId={workspaceId} />
     </div>
   )
 }
 
-export default dashboardPage
+export default DashboardPage
